e0513d7e1d1d1644a491618059582e12af47dd2d
omar mohamed <omarmohhameed828@gmail.com>
2024-01-03 19:37:47 +0200

    setup digital filter

--- controllers/signal_viewer.py
+++ controllers/signal_viewer.py
@@ -4,12 +4,14 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtCore import Qt, QSize
 import pyqtgraph as pg
 from models.signal import Signal
-from managers.signal_loader import ISignalLoader, TextSignalLoader, CSVSignalLoader, ExcelXSignalLoader, ExcelSignalLoader
+from managers.signal_loader import ISignalLoader, TextSignalLoader, CSVSignalLoader, ExcelXSignalLoader, \
+    ExcelSignalLoader
 from helper.get_signal_from_file import get_signal_from_file
 from math import floor
 
+
 class SignalViewer:
-    def __init__(self,window):
+    def __init__(self, window):
         self.signal = None
         self.timer = QTimer()
         self.window = window
@@ -29,11 +31,11 @@ class SignalViewer:
     def _initialize_signals_slots(self):
         self.window.pushButton_6.clicked.connect(self.import_signal)
         self.timer.timeout.connect(self.update_plot)
-    
+
     def import_signal(self):
         self.signal = get_signal_from_file(self.window)
         self.render_signal_to_channel(signal=self.signal)
-    
+
     def render_signal_to_channel(self, signal):
         self.timer.start(floor(8 / self.speed))  # Update every 8/speed ms
         self.x_data.extend(signal.x_vec)
@@ -42,19 +44,13 @@ class SignalViewer:
         self.window.signal_input_graph.plot(self.x_data, self.y_data)
         self.data_index = 0
         self.is_plotting = True
-        
-        
+
     def update_plot(self):
         if self.is_plotting:
             if self.data_index < len(self.largest_x_data):
                 x_data = self.largest_x_data[: self.data_index + 1]
-                # if x_data[-1] < 1:
-                #     self.window.signal_input_graph.setXRange(0, 1)
-                # else:
                 self.window.signal_input_graph.setXRange(x_data[-1] - 1, x_data[-1])
-                # self.window.signal_input_graph.plot(x_data, self.y_data[: self.data_index + 1])
                 self.data_index += 1
             else:
                 self.is_plotting = False
                 self.timer.stop()
-        
--- models/filter.py
+++ models/filter.py
@@ -1,33 +1,141 @@
-from typing import List
-from enum import Enum
-
-from models.signal import Signal
-
-
-class PointType(Enum):
-    ZERO = "zero"
-    POLE = "pole"
+import numpy as np
+from scipy import signal
 
 
 class Filter:
-    def __init__(self, coefficients: List[float]) -> None:
+    def __init__(self, zeros=None, poles=None, gain=1) -> None:
         """
-        Initialize the Filter with the given coefficients.
-
-        Args:
-        coefficients (List[float]): The coefficients of the digital filter.
+        Initialize the Filter class with zeros, poles, and gain.
+        If zeros and poles are not given, they are set to an empty list.
+        The default value for gain is 1.
         """
-        self.coefficients = coefficients
+        self.__zeros = zeros if zeros else []
+        self.__poles = poles if poles else []
+        self.__all_pass = []
+        self.__gain = gain
 
-    def apply(self, signal: Signal) -> Signal:
+    # Define getters
+    def get_zeros(self):
         """
-        Apply the digital filter to the given signal.
-
-        Args:
-        signal (Signal): The input signal to filter.
-
-        Returns:
-        Signal: The filtered output signal.
+        Get the zeros of the filter.
         """
-        # Implementation of filter logic goes here
-        pass
+        return self.__zeros[:]
+
+    def get_poles(self):
+        """
+        Get the poles of the filter.
+        """
+        return self.__poles[:]
+
+    def get_gain(self):
+        """
+        Get the gain of the filter.
+        """
+        return self.__gain
+
+    def get_all_pass(self):
+        """
+        Set the all-passes of the filter.
+        """
+        return self.__all_pass[:]
+
+    # Define setters
+    def set_zeros(self, zeros: list) -> None:
+        """
+        Set the zeros of the filter.
+        """
+        self.__zeros = [*self.__zeros, *zeros]
+
+    def set_poles(self, poles: list) -> None:
+        """
+        Set the poles of the filter.
+        """
+        self.__poles = [*self.__poles, *poles]
+
+    def set_gain(self, gain: float) -> None:
+        """
+        Set the gain of the filter.
+        """
+        self.__gain = gain
+
+    # Define instance methods
+    def response(self, w=None) -> tuple:
+        """
+        Get the response of the filter at a given frequency (w).
+        If w is not given, compute the response at a range of frequencies.
+        Returns a tuple of w (frequency), magnitude, and phase.
+        """
+        if w is None:
+            w, response = signal.freqz_zpk(self.__zeros, self.__poles, self.__gain)
+            # `w` is the x_axis from 0 hz to fmax hz (default value normalized from 0 to pi)
+            # `response` is the complex output of z_transform where we can get the magnitude & phase
+        else:
+            response = signal.zpk_eval_response(self.__zeros, self.__poles, self.__gain, w)
+        magnitude = 20 * np.log10(np.abs(response))
+        # convert from hz into decibels
+        phase = np.unwrap(np.angle(response))  # `np.unwrap` to remove phase discontinuities
+        return w, magnitude, phase
+
+    def add_all_pass(self, a: complex):
+        """
+        Add one all-pass filter with coefficient a to the filter.
+        """
+        self.__all_pass.append(a)
+        self.__poles.append(a)
+        self.__zeros.append(1 / np.conj(a))
+
+    def add_list_all_pass(self, a_list: list):
+        """
+        Add one or more all-pass filters with coefficient a to the filter.
+        """
+        self.__all_pass = a_list.copy()
+        self.__poles = [*self.__poles, *a_list]
+        b_list = []
+        for i in range(len(a_list)):
+            b_list.append(1 / np.conj(a_list[i]))
+        self.__zeros = [*self.__zeros, *b_list]
+
+    def remove_zero(self, zero: complex):
+        """
+        Remove one zero from the filter.
+        """
+        self.__zeros.remove(zero)
+
+    def remove_pole(self, pole: complex):
+        """
+        Remove one pole from the filter.
+        """
+        self.__poles.remove(pole)
+
+    def remove_all_zeros(self):
+        """
+        Remove all zeros from the filter.
+        """
+        self.__zeros = []
+
+    def remove_all_poles(self):
+        """
+        Remove all poles from the filter.
+        """
+        self.__poles = []
+
+    def remove_all_pass(self, a: complex):
+        """
+        Remove one all-pass filter with coefficient a to the filter.
+        """
+        self.__all_pass.remove(a)
+        self.__poles.remove(a)
+        self.__zeros.remove(1 / np.conj(a))
+
+    def remove_all_all_passes(self):
+        """
+        Remove all all-pass filters from the filter.
+        """
+        self.__zeros = self.__zeros[0:len(self.__zeros) - len(self.__all_pass)]
+        self.__poles = self.__poles[0:len(self.__poles) - len(self.__all_pass)]
+        self.__all_pass = []
+
+    def apply_filter(self, values: list) -> []:
+        numerator, denominator = signal.zpk2tf(self.__zeros, self.__poles, self.__gain)
+        filtered_signal = np.real(signal.lfilter(numerator, denominator, values))
+        return filtered_signal
